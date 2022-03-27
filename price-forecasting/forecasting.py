da9a3bafa29e0644daaba08450ae7e3e53d10a6d
frenoid <frenoid@gmail.com>
2022-03-28 02:24:33 +0800

    Split data into training and test dataset

--- price-forecasting/forecasting.py
+++ price-forecasting/forecasting.py
@@ -1,7 +1,49 @@
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
+from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, LongType, StructField
 from pyspark.sql.window import Window
+from pyspark.sql.functions import col
+
+def TimeSeriesSplit(df_m, splitRatio, sparksession):
+
+    # Splitting data into train and test
+    # we maintain the time-order while splitting
+    # if split ratio = 0.7 then first 70% of data is train data
+    # and remaining 30% of data is test data
+    newSchema  = StructType(df_m.schema.fields + \
+                [StructField("Row Number", LongType(), False)])
+    new_rdd = df_m.rdd.zipWithIndex().map(lambda x: list(x[0]) + [x[1]])
+    df_m2 = sparksession.createDataFrame(new_rdd, newSchema)
+    total_rows = df_m2.count()
+    splitFraction  =int(total_rows*splitRatio)
+    df_train = df_m2.where(df_m2["Row Number"] >= 0)\
+                   .where(df_m2["Row Number"] <= splitFraction)
+    df_test = df_m2.where(df_m2["Row Number"] > splitFraction)
+    
+    return df_train, df_test
+
+    # def CheckStationarity(timeSeriesCol):
+    #     # this function works with Pandas dataframe only not with spark dataframes
+    #     # this performs Augmented Dickey-Fuller's test
+    #
+    #     test_result = adfuller(timeSeriesCol.values)
+    #     print("ADF Statistic: % f \n" % test_result[0])
+    #     print("p - value: % f \n"" % test_result[1]")
+    #     print("Critical values are: \n")
+    #     print(test_result[4])
+    #
+    # return df_train, df_test
+
+def Predict(I, df1, df2, timeSeriesCol, predictionCol, joinCol):
+    
+    # this converts differenced predictions to raw predictions
+    dZCol = "DeltaZ" + str(i) 
+    f_strCol = "forecast_"+str(i)+"day"
+    df = df1.join(df2, [joinCol], how="inner")\
+                            .orderBy(asc("Date"))
+    df = df.withColumnRenamed(predictionCol, dZCol)
+    df = df.withColumn(f_strCol, col(dZCol)+col(timeSeriesCol))
+    return df
 
 def main():
     spark = SparkSession. \
@@ -42,6 +84,64 @@ def main():
     |    max|2022-02-19|67549.734375|   68789.625|  66382.0625|67566.828125|67566.828125|350967941479|
     +-------+----------+------------+------------+------------+------------+------------+------------+
     """
+
+    date_window = Window.partitionBy().orderBy("Date")
+    btc_usd_lag = btc_usd.withColumn("PrevDayOpen",
+                                     F.lag(col("Open")).over(date_window))
+    btc_usd_lag.show()
+    """
+    +----------+----------+----------+----------+----------+----------+--------+-----------+
+    |      Date|      Open|      High|       Low|     Close| Adj Close|  Volume|PrevDayOpen|
+    +----------+----------+----------+----------+----------+----------+--------+-----------+
+    |2014-09-17|465.864014|468.174011|452.421997|457.334015|457.334015|21056800|       null|
+    |2014-09-18|456.859985|456.859985|413.104004|424.440002|424.440002|34483200| 465.864014|
+    |2014-09-19|424.102997|427.834991|384.532013| 394.79599| 394.79599|37919700| 456.859985|
+    |2014-09-20|394.673004| 423.29599|389.882996|408.903992|408.903992|36863600| 424.102997|
+    |2014-09-21|408.084991|412.425995|   393.181|398.821014|398.821014|26580100| 394.673004|
+    """
+
+    btc_usd_lag_diff = btc_usd_lag \
+                        .withColumn("diff", F.when(F.isnull(col("Open") - col("PrevDayOpen")), 0).otherwise(col("Open") - col("PrevDayOpen")))
+    btc_usd_lag_diff.show()
+
+    btc_usd_date_diff = btc_usd_lag_diff.select("Date", "diff")
+
+    train_df, test_df = TimeSeriesSplit(btc_usd_date_diff, 0.7, spark)
+
+    train_df.show()
+    # +----------+-------------------+----------+
+    # | Date | diff | Row
+    # Number |
+    # +----------+-------------------+----------+
+    # | 2014 - 0
+    # 9 - 17 | 0.0 | 0 |
+    # | 2014 - 0
+    # 9 - 18 | -9.004029000000003 | 1 |
+    # | 2014 - 0
+    # 9 - 19 | -32.75698799999998 | 2 |
+    # | 2014 - 0
+    # 9 - 20 | -29.429993000000024 | 3 |
+    # | 2014 - 0
+    # 9 - 21 | 13.41198700000001 | 4 |
+    # | 2014 - 0
+    # 9 - 22 | -8.984984999999995 | 5 |
+    # | 2014 - 0
+    # 9 - 23 | 2.9920040000000085 | 6 |
+
+    test_df.show()
+    # +----------+-------------------+----------+
+    # | Date | diff | Row
+    # Number |
+    # +----------+-------------------+----------+
+    # | 2019 - 11 - 30 | 297.33007799999996 | 1900 |
+    # | 2019 - 12 - 01 | -192.44091800000024 | 1901 |
+    # | 2019 - 12 - 02 | -147.58007799999996 | 1902 |
+    # | 2019 - 12 - 03 | -100.06054700000004 | 1903 |
+    # | 2019 - 12 - 04 | -3.8505859999995664 | 1904 |
+
+    # TODO: https://databricks.com/blog/2021/04/06/fine-grained-time-series-forecasting-at-scale-with-facebook-prophet-and-apache-spark-updated-for-spark-3.html
+
+
 
 
 if __name__ == "__main__":
