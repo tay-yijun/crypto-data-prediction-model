2afc0a9581099152eb450d9110bab8d11fa69680
Yijun Tay <tay.yijun@gmail.com>
2022-03-30 09:13:36 +0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.85a040ab19603872577b649e45d4b4aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eef5c79fa7d90f1b55a9",
+    "url": "/crypto-data-prediction-model/static/css/main.92566e4f.chunk.css"
+  },
+  {
+    "revision": "eef5c79fa7d90f1b55a9",
+    "url": "/crypto-data-prediction-model/static/js/main.eef5c79f.chunk.js"
+  },
+  {
+    "revision": "9b61cfc952eff2a7f02b",
+    "url": "/crypto-data-prediction-model/static/js/runtime~main.9b61cfc9.js"
+  },
+  {
+    "revision": "a172aef20322ffb1abf4",
+    "url": "/crypto-data-prediction-model/static/js/2.a172aef2.chunk.js"
+  },
+  {
+    "revision": "27f79f39d4ceb9b9e64afba2d896cf1f",
+    "url": "/crypto-data-prediction-model/index.html"
+  }
+];
